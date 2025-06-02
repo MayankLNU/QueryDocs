baf0db851fdca736daeff099100db3920b2f7ae9
MayankLNU <Mayank.LNU5@cognizant.com>
2025-06-02 16:02:09 +0530

    Upload file POST method in API

--- Backend/app.py
+++ Backend/app.py
@@ -1,3 +1,12 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify
 
 app = Flask(__name__)
+
+@app.route("/upload", methods=["Post"])
+def upload_file():
+    if "file" not in request.files:
+        return abort(400, "No file uploaded")
+    
+    file = request.files["file"]
+
+    return jsonify({"message": f"File {file.filename} uploaded successfully!"}), 200
